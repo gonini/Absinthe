310ec6d84b52b2fd512c449317638396405c77d1
goni <goni>
2020-03-15 16:06:06 +0900

    색상, 문자열, 이미지 관리 파일 추가

---
+++ Absinthe/AbsintheUI/AbsintheUI/PermissionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PermissionViewController.swift
+//  AbsintheUI
+//
+//  Created by User on 15/03/2020.
+//  Copyright © 2020 gonisoft. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+final class PermissionViewController: UIViewController {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Absinthe/AbsintheUI/AbsintheUI/Resources/Colors.swift
@@ -0,0 +1,21 @@
+//
+//  Colors.swift
+//  AbsintheUI
+//
+//  Created by User on 15/03/2020.
+//  Copyright © 2020 gonisoft. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var turtleGreen: UIColor {
+    return UIColor(red: 113.0 / 255.0, green: 191.0 / 255.0, blue: 68.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+}
