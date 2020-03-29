94979e26eef9a07c55e1bcd1ce3c6cef1124b55c
goni <goni>
2020-03-29 17:55:50 +0900

    Remove header file

---
+++ Absinthe/ViewModelInterface/ViewModelInterface/ViewModelInterface.h
@@ -0,0 +1,19 @@
+//
+//  ViewModelInterface.h
+//  ViewModelInterface
+//
+//  Created by User on 29/03/2020.
+//  Copyright © 2020 gonisoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewModelInterface.
+FOUNDATION_EXPORT double ViewModelInterfaceVersionNumber;
+
+//! Project version string for ViewModelInterface.
+FOUNDATION_EXPORT const unsigned char ViewModelInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModelInterface/PublicHeader.h>
+
+
