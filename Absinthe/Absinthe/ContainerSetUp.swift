3fbad4a24f4820864147c7514f475711ffb20c48
goni <goni>
2020-04-04 16:09:01 +0900

    Setup DI Container

--- Absinthe/Absinthe/ContainerSetUp.swift
+++ Absinthe/Absinthe/ContainerSetUp.swift
@@ -8,12 +8,24 @@
 
 import Foundation
 import AbsintheUI
+import ViewModel
+import Service
 import Swinject
 
 extension AppDelegate {
     func createContainer() -> Container {
         let container = Container()
+        // Intro
         container.register(IntroViewController.self) { _ in IntroViewController() }
+
+        // Permission
+        container.register(PermissionServiceType.self) { _ in PermissionService() }
+        container.register(PermissionViewReactor.self) {
+            PermissionViewReactor(permissionService: $0.resolve(PermissionServiceType.self)!)
+        }
+        container.register(PermissionViewController.self) {
+            PermissionViewController(reactor: $0.resolve(PermissionViewReactor.self)!)
+        }
         return container
     }
 }
